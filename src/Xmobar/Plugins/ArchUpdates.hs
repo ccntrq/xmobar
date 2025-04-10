d853f74b91d2d00760eef67fb0755d99551f574c
Alexander 'ccntrq' Pankoff <ccntrq@screenri.de>
2025-04-10 08:20:15 +0200

    feat: add new PacmanUpdates constructor for ArchUpdates Plugin
    the new PacmanUpdates constructor behaves similar to the ArchUpdates
    constructor while additionally allowing to pass in a custom error
    message for unknown pacman  failures. The default error message of
    `pacman: Unknown cause of failure.` of the ArchUpdates ctor is too
    long for my taste.
    
    The ArchUpdates ctor was modified to delegate to the new PacmanUpdates
    plugin while providing the default error message keeping existing behavior
    intact.
    
    The new constructors  name better represents the Plugin's compatibility
    with all pacman-based distributions, not just Arch.
    
    The docs have been updated to reflect the existence of the new plugin
    and to highlight the similarities between the ArchUpdates and
    PacmanUpdates constructor.

--- src/Xmobar/Plugins/ArchUpdates.hs
+++ src/Xmobar/Plugins/ArchUpdates.hs
@@ -1,41 +1,60 @@
 {-# LANGUAGE CPP #-}
 
 -----------------------------------------------------------------------------
--- |
--- Module      :  Plugins.Monitors.ArchUpdates
--- Copyright   :  (c) 2024 Enrico Maria De Angelis
--- License     :  BSD-style (see LICENSE)
---
--- Maintainer  :  Enrico Maria De Angelis <enricomaria.dean6elis@gmail.com>
--- Stability   :  unstable
--- Portability :  unportable
---
--- An ArchLinux updates availablility plugin for Xmobar
---
+
 -----------------------------------------------------------------------------
 
-module Xmobar.Plugins.ArchUpdates (ArchUpdates(..)) where
+{- |
+Module      :  Plugins.Monitors.ArchUpdates
+Copyright   :  (c) 2024 Enrico Maria De Angelis
+License     :  BSD-style (see LICENSE)
 
-import System.Exit (ExitCode(..))
+Maintainer  :  Enrico Maria De Angelis <enricomaria.dean6elis@gmail.com>
+Stability   :  unstable
+Portability :  unportable
+
+An ArchLinux updates availablility plugin for Xmobar
+-}
+module Xmobar.Plugins.ArchUpdates (
+  ArchUpdates (..),
+) where
+
+import System.Exit (ExitCode (..))
 import System.Process (readProcessWithExitCode)
-import Xmobar.Run.Exec
 import Xmobar.Plugins.Command (Rate)
+import Xmobar.Run.Exec
 
-data ArchUpdates = ArchUpdates (String, String, String) Rate
+data ArchUpdates
+  = ArchUpdates (String, String, String) Rate
+  | PacmanUpdates
+      ( String
+      , String
+      , String
+      , String
+      )
+      Rate
   deriving (Read, Show)
 
 instance Exec ArchUpdates where
-    alias (ArchUpdates _ _) = "arch"
-    rate (ArchUpdates _ r) = r
-    run (ArchUpdates (z, o, m) _) = do
-      (exit, stdout, _) <- readProcessWithExitCode "checkupdates" [] ""
-      return $ case exit of
-        ExitFailure 2 -> z--ero updates
-        ExitFailure 1 -> "pacman: Unknown cause of failure."
-        ExitSuccess -> case length $ lines stdout of
-          0 -> impossible
-          1 -> o
-          n -> m >>= \c -> if c == '?' then show n else pure c
-        _ -> impossible
-        where
-          impossible = error "This is impossible based on pacman manpage"
+  alias (ArchUpdates _ _) = "arch"
+  alias (PacmanUpdates _ _) = "pacman"
+  rate (ArchUpdates _ r) = r
+  rate (PacmanUpdates _ r) = r
+  run = archUpdatesRun
+
+archUpdatesRun :: ArchUpdates -> IO String
+archUpdatesRun au = case au of
+  ArchUpdates (z, o, m) _ -> go (z, o, m, "pacman: Unknown cause of failure.")
+  PacmanUpdates p _ -> go p
+ where
+  go (z, o, m, e) = do
+    (exit, stdout, _) <- readProcessWithExitCode "checkupdates" [] ""
+    return $ case exit of
+      ExitFailure 2 -> z -- ero updates
+      ExitFailure 1 -> e
+      ExitSuccess -> case length $ lines stdout of
+        0 -> impossible
+        1 -> o
+        n -> m >>= \c -> if c == '?' then show n else pure c
+      _ -> impossible
+  impossible = error "This is impossible based on pacman manpage"
